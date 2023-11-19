dc618099cfc4db0e6da5d1262ceab4b6288eaaef
sunhaolou <sunhaolo@uw.edu>
2023-11-19 18:13:33 -0500

    fix typo

--- cpu-base/processor.v
+++ cpu-base/processor.v
@@ -106,17 +106,17 @@ module processor(
 
 	 wire[16:0] immediate;
 	 assign immediate = q_imem[16:0];
-	 
+
 	 wire[26:0] target;
 	 assign target = q_imem[26:0];
-	 
+
 	 // decide all control bits
 	 wire use_rd_as_second_readReg, immediate_type, load_reg_from_memory;
 	 wire is_bne, is_blt, is_j, is_jal, is_jr, is_bex, is_setx;
 	 opcode_control u_opc(opcode, use_rd_as_second_readReg, immediate_type,
-		  wren, ctrl_writeEnable, load_reg_from_memory, is_bne, is_blt, 
+		  wren, ctrl_writeEnable, load_reg_from_memory, is_bne, is_blt,
 		  is_j, is_jal, is_jr, is_bex, is_setx);
-	 
+
 	 // decide readRegs for register file
 	 assign ctrl_readRegA = is_bex ? 5'b11110: rs;
 	 assign ctrl_readRegB = is_bex ? 5'b00000: (use_rd_as_second_readReg ? rd : rt);
@@ -129,11 +129,11 @@ module processor(
 	 wire [31:0] sx_immediate;
 	 extend_sign #(.N_PREV(17), .N_EXTENDED(32)) u_xs(immediate, sx_immediate);
 	 assign data_operandB = immediate_type ? sx_immediate : data_readRegB;
-	 
+
 	 // Decide which alu operation will be used
 	 wire [4:0] alu_operation_type;
 	 decide_alu_operation decider_ao(opcode, raw_aluop, alu_operation_type);
-	 
+
 	 // alu module
 	 wire isNotEqual, isLessThan, alu_overflow;
 	 wire [31:0] alu_output;
@@ -144,7 +144,7 @@ module processor(
 	 wire [31:0] data_writeReg_non_exception;
 	 assign ctrl_writeReg_non_exception = rd;
 	 assign data_writeReg_non_exception = load_reg_from_memory ? q_dmem : alu_output;
-	
+
 	 // The Bypass logic for rstatus_value
 	 // decide if we are handling exception in the current cycle
 	 wire exception_overflow;
@@ -155,12 +155,13 @@ module processor(
 	 wire [31:0] data_writeReg_not_Jtype;
 	 assign ctrl_writeReg_not_Jtype = exception_overflow ? 5'b11110 : ctrl_writeReg_non_exception;
 	 assign data_writeReg_not_Jtype = exception_overflow ? rstatus_value_overflow : data_writeReg_non_exception;
-	
-	 // decide the what data is written into which register for J type 
+
+	 // decide the what data is written into which register for J type
+	 wire [11:0] incremented_pc;
 	 assign ctrl_writeReg = is_jal ? 5'b11111 : (is_setx ? 5'b11110 : ctrl_writeReg_not_Jtype);
 	 assign data_writeReg = is_jal ? {{20{1'b0}}, incremented_pc} : (is_setx ? {{5{1'b0}}, target} : data_writeReg_not_Jtype);
 	 /* TODO : Handling the Dirty write of registers */
-	 
+
 	 // decide address for dmem
 	 assign address_dmem = alu_output[11:0];
 	 /* The logic for program counter*/
@@ -170,7 +171,6 @@ module processor(
 
 	 // determine the pc of next cycle
 	 wire ignored1, ignored2, ignore3, ignore4;
-	 wire [11:0] incremented_pc;
 	 // increment pc: address_imem is the output of pc, input of the adder
 	 RCA #(.SIZE(12)) u_incr(incremented_pc, ignored1, ignored2, address_imem, {{11{1'b0}}, 1'b1}, 1'b0);
 	 // add immediate on it
@@ -191,5 +191,5 @@ module processor(
 	 wire should_jump_to_rd;
 	 assign should_jump_to_rd = is_jr;
 	 assign input_pc = should_jump_to_rd ? data_readRegB[11:0] : target_or_imme_or_incr_pc;
-	 
+
 endmodule
